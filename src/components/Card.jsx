2689c7ce5dbefc921a90e56e91406e1cae195066
Yusuf <baliyusuf675@gmail.com>
2023-02-25 18:36:06 +0300

    end

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import img1 from "../assets/react.svg";
+import "./Card.css";
+import CardTitle from "./CardTitle";
+import { languages } from "../helper/data";
+const Card = () => {
+  return (
+    <div className="box text-center">
+      <div className="title">
+        <img src={img1} alt="" width="160px" />
+      </div>
+      <div className="main">
+        <h1 className="text-light">Languages</h1>
+        <div className="strip"></div>
+        <div className="lang row">
+          {languages.map((item) => (
+            <CardTitle {...item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Card;
---
+++ src/components/CardTitle.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const CardTitle = ({ name, img, options }) => {
+  const [toggle, setToggle] = useState(true);
+  const handleMouseEnter = (e) => {
+    e.target.querySelector("img").style.transform = "scale(0.8)";
+    e.target.style.transform = "rotate(5deg)";
+  };
+  const handleMouseLeave = (e) => {
+    e.target.querySelector("img").style.transform = "scale(1)";
+    e.target.style.transform = "rotate(0deg)";
+  };
+  return (
+    <div
+      className="card col-6"
+      role="button"
+      onClick={() => setToggle(!toggle)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {toggle ? (
+        <div>
+          <img src={img} alt="" width="150px" height="100px" />
+          <h6>{name}</h6>
+        </div>
+      ) : (
+        <div className="cardback">
+          {options.map((item, e) => (
+            <ul key={e}>
+              <li>{item}</li>
+            </ul>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CardTitle;
